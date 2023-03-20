016f3d5d9a7cc13eb9dd5843beedfe364ab1be9d
Olexandr Tolochko <0504fusion@gmail.com>
2023-03-20 09:47:44 +0200

    Update PrivateRoute.js

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -3,7 +3,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 export default function PrivateRoute() {
     const isLoggedIn = JSON.parse(window.localStorage.getItem('userInfo'))
-    console.log('isLoggedIn: ', isLoggedIn);
 
     if (!isLoggedIn) {
         return <Navigate to='login' />
